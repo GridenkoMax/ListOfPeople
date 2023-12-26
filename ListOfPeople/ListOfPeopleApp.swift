d03b45113ce8a890029219702826fdcb286fa4bf
Gridenko Maks <Maksgridenko@ya.ru>
2023-12-26 13:31:40 +0300

    Initial Commit

---
+++ ListOfPeople/ListOfPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfPeopleApp.swift
+//  ListOfPeople
+//
+//  Created by Maxim Gridenko on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListOfPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
